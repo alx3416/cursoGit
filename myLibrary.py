f1ee11bd173a5baba59399b993012281140b97e0
alx3416 <alx3416@hotmail.com>
2023-11-29 09:12:39 -0600

    changing arg names in master

--- myLibrary.py
+++ myLibrary.py
@@ -1,8 +1,8 @@
 import math
 
 
-def getHypotenuse(sideA, sideB):
-    hypotenuse = math.sqrt((sideA ** 2) + (sideB ** 2))
+def getHypotenuse(side1, side2):
+    hypotenuse = math.sqrt((side1 ** 2) + (side2 ** 2))
     return hypotenuse
 
 
