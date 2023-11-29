4583c56fa39b0413170bc450b75dbc1c267daf23
alx3416 <alx3416@hotmail.com>
2023-11-29 07:16:47 -0600

    adding a third function

--- myLibrary.py
+++ myLibrary.py
@@ -4,3 +4,13 @@ import math
 def getHypotenuse(sideA, sideB):
     hypotenuse = math.sqrt((sideA ** 2) + (sideB ** 2))
     return hypotenuse
+
+
+def getRectangleArea(sideA, sideB):
+    area = sideA * sideB
+    return area
+
+
+def getcircleArea(radius):
+    area = math.pi * (radius ** 2)
+    return area
